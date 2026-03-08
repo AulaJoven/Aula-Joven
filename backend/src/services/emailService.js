6583985239cbce7a66bf97734a14cf76d3683263
rodrixdere <rodrixdere@gmail.com>
2026-03-08 10:42:30 -0600

    Fix: not recieving email, User creation lasts way too long

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,11 +1,13 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
+  }
 });
 
 const emailTemplate = (nombre, apellidos, email, tempPassword) => `
